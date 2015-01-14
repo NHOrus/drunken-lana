5c557e61aa54a518b7f1eb72a47ddcbc157b90a2
NHOrus <jy6x2b32pie9@yahoo.com>
2015-01-14 22:09:03 +0300

    Clumsy, but working rulesetting

--- ships.go
+++ ships.go
@@ -5,3 +5,19 @@ type ship struct {
 	Fatness int
 	Numbers int
 }
+
+func newShip(F int, N int) ship {
+	sh := new(ship)
+	sh.Fatness = F
+	sh.Numbers = N
+	return *sh
+}
+
+func allShips() *map[string]ship {
+	s := make(map[string]ship)
+	s["battleship"] = newShip(4, 1)
+	s["cruiser"] = newShip(3, 2)
+	s["destroyer"] = newShip(2, 3)
+	s["cutter"] = newShip(1, 4)
+	return &s
+}
