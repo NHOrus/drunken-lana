45e681a61f15e2090615a06797d16a74164cb15c
NHOrus <jy6x2b32pie9@yahoo.com>
2015-01-16 17:36:13 +0300

    First attempt at drawing things. Error

--- main.go
+++ main.go
@@ -11,4 +11,14 @@ func main() {
 	for val, i := range *shs {
 		fmt.Println(val, i) //test of all things shipy. Will change soon
 	}
+
+	rule, _ := getRules()
+	leftField := newField(rule.fLen, rule.fWid)
+	rightField := newField(rule.fLen, rule.fWid)
+	rfc := make(chan Field)
+	rfc <- *rightField
+	lfc := make(chan Field)
+	lfc <- *leftField
+
+	drawfield(lfc, rfc)
 }
