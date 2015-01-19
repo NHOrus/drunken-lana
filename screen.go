8baf65ded84ec8315b5955ca29d1443a423baa92
NHOrus <jy6x2b32pie9@yahoo.com>
2015-01-19 21:47:40 +0300

    and finished typesafing

--- screen.go
+++ screen.go
@@ -22,22 +22,23 @@ func drawfield(lField <-chan Field, rField <-chan Field) error {
 	//We are getting field parameters. Possibly need to read them from rules, but...
 	lFl = <-lField
 	rFl = <-rField
+
 	//Sanity checks
 	if (lFl.Fheight != rFl.Fheight) && (lFl.Fwidth != rFl.Fwidth) {
 		return errors.New("Can't do mismatched fields yet")
 	}
 
-	sFlen := lFl.Fheight + rFl.Fheight + 1
-	sFwid := lFl.Fwidth + rFl.Fwidth + 3
+	sFhei := int(lFl.Fheight + rFl.Fheight + 1)
+	sFwid := int(lFl.Fwidth + rFl.Fwidth + 3)
 	//one field in Heigth, two fields in Width, plus line for identifications of rows and columns
 
 	//And more sanity checks
-	if (termSizW < sFwid) && (termSizH < sFlen) {
+	if (termSizW < sFwid) && (termSizH < sFhei) {
 		return errors.New("Terminal too small")
 	}
 
 	for varW := (termSizW - sFwid); varW < termSizW; varW++ {
-		for varH := (termSizH - sFlen); varH < termSizH; varH++ {
+		for varH := (termSizH - sFhei); varH < termSizH; varH++ {
 			termbox.SetCell(varW, varH, ' ', termbox.ColorWhite, termbox.ColorBlack)
 
 		}
